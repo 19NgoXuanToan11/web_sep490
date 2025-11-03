8f80e21fa6ad6801e3ace89dea8c96e71917a14b
19NgoXuanToan11 <toannxse171297@fpt.edu.vn>
2025-11-03 07:42:33 +0700

    fix vercel

--- FE/src/app/router.tsx
+++ FE/src/app/router.tsx
@@ -28,7 +28,6 @@ import { ProductsPage } from '@/features/products-management/pages/ProductsPage'
 
 // Payment result page (public)
 import PaymentResultPage from '@/pages/payment/PaymentResultPage'
-import PaymentResultDemo from '@/pages/payment/PaymentResultDemo'
 
 // RBAC components
 const Unauthorized = React.lazy(() => import('@/pages/Unauthorized'))
@@ -51,10 +50,6 @@ const routerConfig = [
   {
     path: '/payment-result',
     element: <PaymentResultPage />,
-  },
-  {
-    path: '/payment-demo',
-    element: <PaymentResultDemo />,
   },
   // {
   //   path: '/register',
